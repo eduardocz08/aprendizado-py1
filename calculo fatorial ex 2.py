fba3fb4d47939d28d67b2fa3ff0e17c2e2cb1de2
eduardocz08 <eduardoacruz80@gmail.com>
2025-07-15 21:18:26 -0300

    Add files via upload

---
+++ calculo fatorial ex 2.py
@@ -0,0 +1,13 @@
+
+
+#exerc 2
+
+def exibir_dados (nome, idade):
+     print(f'Olá, {nome}!Você tem {idade}anos.')
+
+
+
+nome=input('digite seu nome:')
+idade=int(input('digite sua idade:'))
+
+exibir_dados(nome, idade)
